3379ca96f37e505411d257a5dc5e9f2b1165139e
Mark <mark@remarkablemark.org>
2021-09-11 13:46:25 -0400

    chore(packages): add lint

--- packages/bar/src/index.ts
+++ packages/bar/src/index.ts
@@ -1,3 +1,3 @@
-export default function bar() {
+export default function bar(): string {
   return 'bar';
 }
--- packages/foo/src/index.ts
+++ packages/foo/src/index.ts
@@ -1,3 +1,3 @@
-export default function foo() {
+export default function foo(): string {
   return 'foo';
 }
