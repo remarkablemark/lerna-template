2133262bb3559a85911bc8ed3fc4951521d14a0e
Mark <mark@remarkablemark.org>
2021-09-11 14:36:40 -0400

    chore(root): update rules in commitlint.config.js

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,21 @@
+const { readdirSync } = require('fs');
+
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    'body-max-line-length': [1, 'always', 100],
+    'scope-enum': [
+      2,
+      'always',
+      [
+        'all',
+        'deps',
+        'packages',
+        'release',
+        'root',
+        ...readdirSync('./packages'),
+      ],
+    ],
+    'scope-empty': [2, 'never'],
+  },
 };
