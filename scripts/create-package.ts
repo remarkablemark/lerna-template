375c2f19f99cbc6f86868568c8ce09f5abc80ffe
Mark <mark@remarkablemark.org>
2021-09-19 15:41:46 -0400

    refactor(root): use yargs in create-package script

--- scripts/create-package.ts
+++ scripts/create-package.ts
@@ -3,8 +3,10 @@
 /* eslint-disable @typescript-eslint/no-var-requires, no-console */
 
 import { execSync } from 'child_process';
-import { writeFileSync } from 'fs';
+import { existsSync, writeFileSync } from 'fs';
 import { resolve } from 'path';
+import { hideBin } from 'yargs/helpers';
+import yargs from 'yargs/yargs';
 
 /**
  * Executes command.
@@ -17,59 +19,86 @@ const exec = (command: string) => execSync(command, { stdio: 'inherit' });
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const stringify = (data: any) => JSON.stringify(data, null, 2) + '\n';
 
-const [packageName] = process.argv.slice(2);
-
-if (!packageName) {
-  console.error(
-    `Please specify a package name:
-
-  $ yarn create-package <name>
-`
-  );
-  process.exit(1);
-}
+const templateChoices = ['typescript'] as const;
 
 let scope = '';
-let directory = packageName;
+let directory = '';
+let packageName = '';
 
-if (packageName[0] === '@') {
-  const match = packageName.split('/');
-  scope = match[0];
-  directory = match[1];
+const path = {
+  directory: '',
+  packages: resolve(__dirname, '../packages'),
+  root: resolve(__dirname, '..'),
+  template: '',
+  templates: resolve(__dirname, '../templates'),
+};
 
-  if (!/^@[\w-]+$/.test(scope) || !/^[\w-]+$/.test(directory)) {
-    console.error(`Invalid package name: ${packageName}`);
-    process.exit(1);
-  }
-}
+yargs(hideBin(process.argv))
+  .usage('Usage: yarn $0 <name> --template=[string]')
+  .command('<name>', 'Create package name', (yargs) =>
+    yargs.positional('name', {
+      describe: 'package name',
+      type: 'string',
+    })
+  )
+  .demandCommand(1)
+  .option('template', {
+    choices: templateChoices,
+    describe: 'Template type',
+    type: 'string',
+    default: 'typescript',
+  })
+  .check((argv) => {
+    [packageName] = argv._ as string[];
+    directory = packageName;
+
+    if (packageName[0] === '@') {
+      [scope, directory] = packageName.split('/');
+      if (!scope || !/^@[\w-]+$/.test(scope)) {
+        throw new Error(`Invalid package scope: ${scope}`);
+      }
+    }
+
+    if (!directory || !/^[\w-]+$/.test(directory)) {
+      throw new Error(`Invalid package name: ${packageName}`);
+    }
+
+    if (existsSync(resolve(path.packages, directory))) {
+      throw new Error(`Package directory exists: ${directory}`);
+    }
+
+    return true;
+  })
+  .parseSync();
 
 exec('yarn');
 
 const packages = 'packages';
 const template = 'typescript-template';
-const rootPath = resolve(__dirname, '..');
-const templatePath = resolve(rootPath, 'templates', template);
-const directoryPath = resolve(rootPath, packages, directory);
-console.log(`Copying '${templatePath}' to '${directoryPath}'...`);
-exec(`cp -r ${templatePath} ${directoryPath}`);
+
+path.directory = resolve(path.packages, directory);
+path.template = resolve(path.templates, template);
+
+console.log(`Copying '${path.template}' to '${path.directory}'...`);
+exec(`cp -r ${path.template} ${path.directory}`);
 
 console.log(
-  `Replacing string '${template}' with '${packageName}' in '${directoryPath}'...`
+  `Replacing string '${template}' with '${packageName}' in '${path.directory}'...`
 );
 exec(
-  `grep -rl '${template}' '${directoryPath}' | xargs sed -i '' -e 's|${template}|${packageName}|g'`
+  `grep -rl '${template}' '${path.directory}' | xargs sed -i '' -e 's|${template}|${packageName}|g'`
 );
 
 console.log(`Updating '${packageName}' package.json...`);
-const packageJsonPath = resolve(directoryPath, 'package.json');
+const packageJsonPath = resolve(path.directory, 'package.json');
 const packageJson = require(packageJsonPath);
 delete packageJson.private;
 writeFileSync(packageJsonPath, stringify(packageJson));
 
 const releasePleaseConfigFilename = 'release-please-config.json';
-const releasePleaseConfigPath = resolve(rootPath, releasePleaseConfigFilename);
+const releasePleaseConfigPath = resolve(path.root, releasePleaseConfigFilename);
 console.log(
-  `Adding package '${directoryPath}' to '${releasePleaseConfigFilename}'...`
+  `Adding package '${path.directory}' to '${releasePleaseConfigFilename}'...`
 );
 const releasePleaseConfig = require(releasePleaseConfigPath);
 releasePleaseConfig.packages[`${packages}/${directory}`] = {};
